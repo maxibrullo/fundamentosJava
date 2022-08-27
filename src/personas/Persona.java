9254884ba5c85776630de3b8ec614ff428357611
Maximiliano Brullo <maxibrullo@outlook.com>
2022-08-27 18:18:06 -0300

    first commit

---
+++ src/personas/Persona.java
@@ -0,0 +1,19 @@
+package personas;
+
+public class Persona {
+
+    private String nombre;
+    private Integer edad;
+    private Character genero;
+
+    private static idPersona
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, Integer edad, Character genero) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+    }
+}
